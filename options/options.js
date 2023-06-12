bab11e6f92e2d72b245e8560f664749ab0621bcc
Crowdbotics <engineering@crowdbotics.com>
2023-06-12 06:12:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-bird-dev-90642",
+  url: "https://silent-bird-dev-90642.botics.co",
+  api: "https://silent-bird-dev-90642.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
